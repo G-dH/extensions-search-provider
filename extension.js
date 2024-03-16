0dcd1a55db4ca24c6dd7f9b9d2983a96360d55b1
G-dH <georgdh@gmail.com>
2024-03-16 23:56:52 +0100

    extension: Fix synchronous call of potentially async functions

--- extension.js
+++ extension.js
@@ -14,7 +14,6 @@ import GLib from 'gi://GLib';
 
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as Extension from 'resource:///org/gnome/shell/extensions/extension.js';
-import { ExtensionState } from 'resource:///org/gnome/shell/misc/extensionUtils.js';
 
 // Me imports
 import * as Settings from './settings.js';
@@ -27,7 +26,7 @@ let reorderTime = 0;
 
 
 export default class ESP extends Extension.Extension {
-    enable() {
+    async enable() {
         // Reorder extensions to prevent ESP from rebasing when disabling other extensions
         // also reorder V-Shell extension as its rebase hides the overview
         // This option is opt-in so the user can make informed decision
@@ -35,7 +34,7 @@ export default class ESP extends Extension.Extension {
         // in case when something external breaks the extensionOrder during reordering (only similar extension can do this)
 
         // If reordering was successful, ESP is already enabled, so return
-        if (this._reorderExtensions())
+        if (await this._reorderExtensions())
             return;
 
         const Me = {};
@@ -45,7 +44,7 @@ export default class ESP extends Extension.Extension {
         Me.gSettings = this.getSettings();
         Me.Settings = Settings;
         Me.Util = Util;
-        Me.gettext = this.gettext.bind(this);
+        Me._ = this.gettext.bind(this);
         Me.opt = new Me.Settings.Options(Me);
 
         this.Me = Me;
@@ -68,7 +67,7 @@ export default class ESP extends Extension.Extension {
         console.debug(`${this.metadata.name}: disabled`);
     }
 
-    _reorderExtensions() {
+    async _reorderExtensions() {
         // This function is called before initialization of Settings module
         const settings = this.getSettings();
         const reorderEnabled = settings.get_boolean('reorder-extensions');
@@ -89,7 +88,6 @@ export default class ESP extends Extension.Extension {
         }
 
         const callerNameOK = callerName === '_callExtensionEnable';
-
         // Cancel if our stack trace inspection wasn't successful and prevent any potential recursion
         if (!callerNameOK || globalRebasingInProgress || reorderingInProgress || (reorderTime && Date.now() - reorderTime < 1000)) {
             if (!(globalRebasingInProgress || itIsMe))
@@ -100,6 +98,7 @@ export default class ESP extends Extension.Extension {
         reorderingInProgress = true;
         reorderTime = Date.now();
         const extensionManager = Main.extensionManager;
+
         // Extensions that should be enabled
         const enabledExtensions = extensionManager._enabledExtensions;
         // Extensions already enabled, make a copy
@@ -137,6 +136,7 @@ export default class ESP extends Extension.Extension {
             // except for those with more session modes at the beginning of the list
             if (index > lastMultiSessionIndex) {
                 console.log(`[${this.metadata.name}]:  Disabling ${uuid}`);
+                // disable() is called synchronously in the default extensionSystem
                 this._callExtensionDisable(uuid);
                 if (sessionModes > 1) {
                     // move the uuid to multi-session list
@@ -149,27 +149,30 @@ export default class ESP extends Extension.Extension {
         }
 
         // If V-Shell should be enabled, but wasn't enabled before ESP, enable it here if possible
-        // _callVShellEnable is a original _callExtensionEnable, but synchronous
+        // _callExtensionEnable will cancel it if V-Shell is already enabled
         if (vShellUUID)
-            this._callVShellEnable(vShellUUID);
+            await extensionManager._callExtensionEnable.bind(extensionManager)(vShellUUID);
 
         // Re-enable the previously disabled extensions
         // First multi-session ones
         for (const uuid of multiSessionExtensions) {
             console.log(`[${this.metadata.name}]:  Enabling ${uuid}`);
-            this._callExtensionEnable(uuid);
+            // eslint-disable-next-line no-await-in-loop
+            await this._callExtensionEnable(uuid);
         }
 
         // Enable ESP here. We are calling the same enable from enable, but it's handled
         // The extension manager will add ESP to _extensionOrder twice, but we will fix that later
         itIsMe = true; // Just to prevent recursion error log
         console.log(`[${this.metadata.name}]:  Enabling ${this.metadata.uuid}`);
-        this._callExtensionEnable(this.metadata.uuid);
+        await this._callExtensionEnable(this.metadata.uuid);
         itIsMe = false;
 
+        // Re-enable the rest of the extensions
         for (const uuid of extensionOrder) {
             console.log(`[${this.metadata.name}]:  Enabling ${uuid}`);
-            this._callExtensionEnable(uuid);
+            // eslint-disable-next-line no-await-in-loop
+            await this._callExtensionEnable(uuid);
         }
 
         // Extension manager will add the ESP uuid once again, after the extensions that we re-enabled here
@@ -186,73 +189,41 @@ export default class ESP extends Extension.Extension {
             else // This error indicates error in this function and should never be needed in the final version
                 console.error(`${this.metadata.name}: The duplicate ESP uuid was not found on the expected position in the _extensionOrder, something has failed...`);
 
+            // Reorder enabled-extensions key since its order is followed while unlocking screen or reenabling user-extensions
+            const enabledExtensionsKey = global.settings.get_strv('enabled-extensions');
+            enabledExtensionsKey.sort((a, b) => (b.includes(this.metadata.uuid) && a !== vShellUUID && enabledExtensionsKey.indexOf(b) > enabledExtensionsKey.indexOf(a)) || b === vShellUUID);
+            // Move extensions supporting more session modes at the beginning
+            // to minimize unnecessary disable/enable cycles during the first screen lock/unlock (default from GS 46)
+            enabledExtensionsKey.sort((a, b) => {
+                const sessionModesA = extensionManager.lookup(a)?.sessionModes || [];
+                const sessionModesB = extensionManager.lookup(b)?.sessionModes || [];
+                return sessionModesA < sessionModesB;
+            });
+            global.settings.set_strv('enabled-extensions', enabledExtensionsKey);
+
             return GLib.SOURCE_REMOVE;
         });
-
-        // Reorder enabled-extensions key since its order is followed while unlocking screen or reenabling user-extensions
-        const enabledExtensionsKey = global.settings.get_strv('enabled-extensions');
-        enabledExtensionsKey.sort((a, b) => (b.includes(this.metadata.uuid) && a !== vShellUUID && enabledExtensionsKey.indexOf(b) > enabledExtensionsKey.indexOf(a)) || b === vShellUUID);
-        // Move extensions supporting more session modes at the beginning
-        // to minimize unnecessary disable/enable cycles during the first screen lock/unlock (default from GS 46)
-        enabledExtensionsKey.sort((a, b) => {
-            const sessionModesA = extensionManager.lookup(a)?.sessionModes || [];
-            const sessionModesB = extensionManager.lookup(b)?.sessionModes || [];
-            return sessionModesA < sessionModesB;
-        });
-        global.settings.set_strv('enabled-extensions', enabledExtensionsKey);
 
         reorderingInProgress = false;
         return true;
     }
 
-    _callExtensionEnable(uuid) {
+    async _callExtensionEnable(uuid) {
         try {
-            Main.extensionManager.lookup(uuid).stateObj.enable();
+            await Main.extensionManager.lookup(uuid).stateObj.enable();
         } catch (e) {
-            Main.extensionManager.logExtensionError(uuid, e);
+            Main.extensionManager.bind(Main.extensionManager).logExtensionError(uuid, e);
         }
         Main.extensionManager._extensionOrder.push(uuid);
     }
 
+    // disable() is called synchronously in the default extensionSystem
     _callExtensionDisable(uuid) {
         try {
             Main.extensionManager.lookup(uuid).stateObj.disable();
         } catch (e) {
-            Main.extensionManager.logExtensionError(uuid, e);
+            Main.extensionManager.bind(Main.extensionManager).logExtensionError(uuid, e);
         }
         Main.extensionManager._extensionOrder.pop();
-    }
-
-    _callVShellEnable(uuid) {
-        const extensionManager = Main.extensionManager;
-        const extension = extensionManager.lookup(uuid);
-
-        if (!extension)
-            return;
-
-        if (extension.state !== ExtensionState.DISABLED)
-            return;
-
-        console.log(`[${this.metadata.name}]:  Enabling ${uuid}`);
-
-        extension.state = ExtensionState.ENABLING;
-        extensionManager.emit('extension-state-changed', extension);
-
-        try {
-            extensionManager._loadExtensionStylesheet(extension);
-        } catch (e) {
-            extensionManager.logExtensionError(uuid, e);
-            return;
-        }
-
-        try {
-            extension.stateObj.enable();
-            extension.state = ExtensionState.ENABLED;
-            extensionManager._extensionOrder.push(uuid);
-            extensionManager.emit('extension-state-changed', extension);
-        } catch (e) {
-            extensionManager._unloadExtensionStylesheet(extension);
-            extensionManager.logExtensionError(uuid, e);
-        }
     }
 }
